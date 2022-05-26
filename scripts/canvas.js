76f133ca00e34c150728ab53b3d654dc188fe0db
Litten 1up <seant9140@gmail.com>
2022-05-26 14:12:28 -0500

    new file:   6th-grade.html 	new file:   7th-grade.html 	new file:   CNAME 	new file:   css/styles.css 	new file:   img/bitten.gif 	new file:   img/email.png 	new file:   img/github.png 	new file:   img/twitter.png 	new file:   img/youtube.png 	new file:   index.html 	new file:   scripts/canvas.js 	new file:   scripts/dropdown.js

---
+++ scripts/canvas.js
@@ -0,0 +1,7 @@
+var c = document.getElementById("canvas");
+var ctx = c.getContext("2d");
+ctx.font = "10px Arial";
+ctx.fillText("so sense I don't want to dox myself, I'm not putting the website here", 0, 10);
+ctx.beginPath();
+ctx.arc(95, 60, 40, 0, 2 * Math.PI);
+ctx.stroke();
---
+++ scripts/dropdown.js
@@ -0,0 +1,32 @@
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function dropdown() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+  
+  // Close the dropdown if the user clicks outside of it
+  window.onclick = function(e) {
+    if (!e.target.matches('.dropbtn')) {
+    var myDropdown = document.getElementById("myDropdown");
+      if (myDropdown.classList.contains('show')) {
+        myDropdown.classList.remove('show');
+      }
+    }
+  }
+  // When the user scrolls the page, execute myFunction
+window.onscroll = function() {sticky};
+
+// Get the navbar
+var navbar = document.getElementById("navbar");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function sticky() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
